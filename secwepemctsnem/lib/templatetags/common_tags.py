ca0f82d0fd335c1b3d49c2fb9feb6b2674db742b
Neskie Manuel <neskiem@gmail.com>
2010-07-14 08:47:49 -0700

    added rss feed tag

--- secwepemctsnem/lib/templatetags/common_tags.py
+++ secwepemctsnem/lib/templatetags/common_tags.py
@@ -1,10 +1,29 @@
-## tags.py
+import feedparser #feedparser.org
+import datetime
 from django import template
 register = template.Library()
-## tags.py
+
 @register.simple_tag
 def active(request, pattern):
     import re
     if re.search(pattern, request):
         return 'active'
     return request
+
+@register.inclusion_tag('feedreader.html')
+def pull_feed(feed_url, posts_to_show=5):
+    feed = feedparser.parse(feed_url)
+    posts = []
+    try:
+        for i in range(posts_to_show):
+            pub_date = feed['entries'][i].updated_parsed
+            published = datetime.date(pub_date[0], pub_date[1], pub_date[2] )
+            posts.append({
+                'title': feed['entries'][i].title,
+                'summary': feed['entries'][i].summary,
+                'link': feed['entries'][i].link,
+                'date': published,
+                })
+    except IndexError:
+        pass
+    return {'posts': posts}
