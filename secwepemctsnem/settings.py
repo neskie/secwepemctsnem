ccf1401294d8a7c924d6f72da526b45a0ca2ede2
Neskie Manuel <neskiem@gmail.com>
2010-07-14 08:54:02 -0700

    added template context processers

--- secwepemctsnem/settings.py
+++ secwepemctsnem/settings.py
@@ -56,6 +56,12 @@ INSTALLED_APPS = (
     'tagging',
     'lib',
 )
+TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
+            "django.core.context_processors.debug",
+            "django.core.context_processors.i18n",
+            "django.core.context_processors.media",
+            "django.contrib.messages.context_processors.messages",
+            "word.context_processors.variables",)
 
 # Tagging Options
 FORCE_LOWERCASE_TAGS = True
