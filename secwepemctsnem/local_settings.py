a8a6c7bca19455e69ec39694b79b42c823555a9e
Neskie Manuel <neskiem@gmail.com>
2010-07-14 22:51:39 -0700

    remove localsettings from repository

--- secwepemctsnem/local_settings.py
+++
@@ -1,8 +0,0 @@
-import os
-PROJ_ROOT = os.path.dirname(os.path.realpath(__file__))
-DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2' or 'sqlite3'.
-DATABASE_NAME = PROJ_ROOT+'/secwepemctsnem.db'             
-DATABASE_USER = ''             
-DATABASE_PASSWORD = ''         
-DATABASE_HOST = ''             
-DATABASE_PORT = ''             
